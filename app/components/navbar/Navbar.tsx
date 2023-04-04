ae5e6156e71881a38bbdb3a4335a848d577ec3f4
c4chris124 <c4chris124@gmail.com>
2023-04-04 13:20:11 -0600

    Created nav bar components, avatar, search, usermenu, added public imgs

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,7 @@
 import { Container } from "../Container"
 import Logo from "./Logo"
+import Search from "./Search"
+import UserMenu from "./UserMenu"
 
 export const Navbar = () => {
     return (
@@ -15,6 +17,8 @@ export const Navbar = () => {
                     md:gap-0
                     ">
                         <Logo />
+                        <Search />
+                        <UserMenu />
                     </div>
                 </Container>
             </div>
