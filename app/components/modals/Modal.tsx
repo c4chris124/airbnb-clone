451f4074e67d376fa3df706c4cb08bd2a7c03441
c4chris124 <c4chris124@gmail.com>
2023-04-07 15:29:38 -0600

    Added footer

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -182,6 +182,7 @@ const Modal: React.FC<ModalProps> = ({
                                         onClick={handleSubmit}
                                     />
                                 </div>
+                                {footer}
                             </div>
 
                         </div>
